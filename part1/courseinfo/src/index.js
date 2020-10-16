2343633b3294e476c11a1ca78f1694a570d968cb
fachunka <kevanmurtagh@gmail.com>
2020-10-16 18:35:49 +0300

    exercises 1.1 - 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -9,18 +9,24 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <div>
       <p>
         {props.part1} {props.exercises1}
-      </p>
-      <p>
         {props.part2} {props.exercises2}
-      </p>
-      <p>
         {props.part3} {props.exercises3}
       </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part1={props.part1} exercises1={props.exercises1}/>
+      <Part part2={props.part2} exercises2={props.exercises2}/>
+      <Part part3={props.part2} exercises3={props.exercises3}/>
     </div>
   )
 }
@@ -45,13 +51,12 @@ const App = () => {
     <div>
       <Header course={course}/>
       <Content 
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+       part1={part1}
+       part2={part2}
+       part3={part3}
+       exercises1={exercises1}
+       exercises2={exercises2}
+       exercises3={exercises3}/>
       <Total 
         exercises1={exercises1}
         exercises2={exercises2}
